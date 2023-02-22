8aace7232b30163309cb58da5a193b6907b1c236
Pablo Acosta <acostaortizpablo@gmail.com>
2023-02-22 16:52:57 +0100

    .

--- resources/views/inicio.blade.php
+++ resources/views/inicio.blade.php
@@ -20,23 +20,12 @@
 
 </style>
 
-{{-- 
-@foreach ($posts as $post)
-        <div>
-            <h1>{{$post->titulo}}</h1>
-            <img src="{{$post->img}}" alt="">
-            <span class="">user: {{$post->user->name}}</span>
-            <img src="{{$post->user->profile->img}}" alt="" width="50" height="50">
-            <span class="">Fecha: {{$post->created_at}}</span>
-        </div>
-@endforeach --}}
-
 <div class="container p-4 rounded d-flex flex-column justify-content-center">{{-- Div fondo --}}
     @foreach ($posts as $post)
         <div class="post container-fluid w-100 mx-auto mb-5"> {{-- Div Card --}}
             <div class="bg-dark-subtle rounded-top">
                 <div class="d-flex align-items-center gap-3 ms-4">
-                    <img src="{{asset($post->user->profile->img)}}" alt="imagenUsuario" width="50px" height="50px" class="img-fluid rounded-circle">
+                    <img src="{{asset($post->user->profile->img)}}" alt="imagenUsuario" width="50px" height="50px" class="rounded-circle">
                     <div class="mt-3 d-flex flex-column align-items-center justify-content-center">
                         <h5>{{$post->user->name}}</h5>
                         <p>{{$post->created_at}}</p>
