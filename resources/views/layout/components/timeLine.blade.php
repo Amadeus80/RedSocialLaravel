2bcdf77af5f6bc24ee65a6982c33de3cfa7865bb
AdrianTeru2001 <adrianteru2001@gmail.com>
2023-02-22 12:45:15 +0100

    Actualizaciones TimeLine

--- resources/views/layout/components/timeLine.blade.php
+++ resources/views/layout/components/timeLine.blade.php
@@ -1,33 +1,250 @@
 <style>
-    #post{
-        width: 100%;
+
+    .tituloPost{
+        text-overflow: ellipsis ;
+        white-space: nowrap;
+        overflow: hidden;
     }
-    @media (min-width: 1000px) {
-        #post{
-            width: 50%;
+
+    @media (min-width: 992px) {
+        .post{
+            width: 50% !important;
         }
     }
+
 </style>
 
-<div class="container bg-white p-4 rounded d-flex flex-column justify-content-center">
+<div class="container p-4 rounded d-flex flex-column justify-content-center">{{-- Div fondo --}}
 
-    <div id="post" class="bg-dark-subtle m-auto rounded-top">
-        <div class="d-flex align-items-center gap-3 ms-4">
-            <img src="img/default_profile/default_avatar.png" alt="imagenUsuario" width="50px" height="50px" class="img-fluid rounded-circle">
-            <div class="mt-3 d-flex flex-column align-items-center justify-content-center">
-                <h5>Lionel Messi</h5>
-                <p>19/abril/2023</p>
+    <div class="post container-fluid w-100 mx-auto mb-5"> {{-- Div Card --}}
+
+        <div class="bg-dark-subtle rounded-top">
+            <div class="d-flex align-items-center gap-3 ms-4">
+                <img src="img/default_profile/default_avatar.png" alt="imagenUsuario" width="50px" height="50px" class="img-fluid rounded-circle">
+                <div class="mt-3 d-flex flex-column align-items-center justify-content-center">
+                    <h5>Lionel Messi</h5>
+                    <p>19/abril/2023</p>
+                </div>
             </div>
         </div>
-    </div>
+    
+        <div class="m-auto">
+            <img src="img/default_profile/default_avatar.png" alt="imagen" class="img-fluid w-100">
+        </div>
+    
+        <div class="bg-dark-subtle m-auto rounded-bottom p-2 d-flex align-items-center">
+            <i class="icono bi bi-heart fs-2 mx-2"></i>
+            <i class="icono bi bi-chat fs-2 mx-2"></i>
+            <h4 class="tituloPost m-auto p-1">adrian teruel reina es mi nombre pero arroba pone messi liones</h4>
+        </div>
 
-    <div id="post" class="m-auto">
-        <img src="img/default_profile/default_avatar.png" alt="imagen" class="img-fluid w-100">
     </div>
+    <div class="post container-fluid w-100 mx-auto mb-5"> {{-- Div Card --}}
 
-    <div id="post" class="bg-dark-subtle m-auto rounded-bottom p-2">
-        <i class="bi bi-heart fs-2 mx-2"></i>
-        <i class="bi bi-chat fs-2 mx-2"></i>
+        <div class="bg-dark-subtle rounded-top">
+            <div class="d-flex align-items-center gap-3 ms-4">
+                <img src="img/default_profile/default_avatar.png" alt="imagenUsuario" width="50px" height="50px" class="img-fluid rounded-circle">
+                <div class="mt-3 d-flex flex-column align-items-center justify-content-center">
+                    <h5>Lionel Messi</h5>
+                    <p>19/abril/2023</p>
+                </div>
+            </div>
+        </div>
+    
+        <div class="m-auto">
+            <img src="img/default_profile/default_avatar.png" alt="imagen" class="img-fluid w-100">
+        </div>
+    
+        <div class="bg-dark-subtle m-auto rounded-bottom p-2 d-flex align-items-center">
+            <i class="icono bi bi-heart fs-2 mx-2"></i>
+            <i class="icono bi bi-chat fs-2 mx-2"></i>
+            <h4 class="tituloPost m-auto p-1">adrian teruel reina es mi nombre pero arroba pone messi liones</h4>
+        </div>
+
     </div>
+    <div class="post container-fluid w-100 mx-auto mb-5"> {{-- Div Card --}}
 
+        <div class="bg-dark-subtle rounded-top">
+            <div class="d-flex align-items-center gap-3 ms-4">
+                <img src="img/default_profile/default_avatar.png" alt="imagenUsuario" width="50px" height="50px" class="img-fluid rounded-circle">
+                <div class="mt-3 d-flex flex-column align-items-center justify-content-center">
+                    <h5>Lionel Messi</h5>
+                    <p>19/abril/2023</p>
+                </div>
+            </div>
+        </div>
+    
+        <div class="m-auto">
+            <img src="img/default_profile/default_avatar.png" alt="imagen" class="img-fluid w-100">
+        </div>
+    
+        <div class="bg-dark-subtle m-auto rounded-bottom p-2 d-flex align-items-center">
+            <i class="icono bi bi-heart fs-2 mx-2"></i>
+            <i class="icono bi bi-chat fs-2 mx-2"></i>
+            <h4 class="tituloPost m-auto p-1">adrian teruel reina es mi nombre pero arroba pone messi liones</h4>
+        </div>
+
+    </div>
+    <div class="post container-fluid w-100 mx-auto mb-5"> {{-- Div Card --}}
+
+        <div class="bg-dark-subtle rounded-top">
+            <div class="d-flex align-items-center gap-3 ms-4">
+                <img src="img/default_profile/default_avatar.png" alt="imagenUsuario" width="50px" height="50px" class="img-fluid rounded-circle">
+                <div class="mt-3 d-flex flex-column align-items-center justify-content-center">
+                    <h5>Lionel Messi</h5>
+                    <p>19/abril/2023</p>
+                </div>
+            </div>
+        </div>
+    
+        <div class="m-auto">
+            <img src="img/default_profile/default_avatar.png" alt="imagen" class="img-fluid w-100">
+        </div>
+    
+        <div class="bg-dark-subtle m-auto rounded-bottom p-2 d-flex align-items-center">
+            <i class="icono bi bi-heart fs-2 mx-2"></i>
+            <i class="icono bi bi-chat fs-2 mx-2"></i>
+            <h4 class="tituloPost m-auto p-1">adrian teruel reina es mi nombre pero arroba pone messi liones</h4>
+        </div>
+
+    </div>
+    <div class="post container-fluid w-100 mx-auto mb-5"> {{-- Div Card --}}
+
+        <div class="bg-dark-subtle rounded-top">
+            <div class="d-flex align-items-center gap-3 ms-4">
+                <img src="img/default_profile/default_avatar.png" alt="imagenUsuario" width="50px" height="50px" class="img-fluid rounded-circle">
+                <div class="mt-3 d-flex flex-column align-items-center justify-content-center">
+                    <h5>Lionel Messi</h5>
+                    <p>19/abril/2023</p>
+                </div>
+            </div>
+        </div>
+    
+        <div class="m-auto">
+            <img src="img/default_profile/default_avatar.png" alt="imagen" class="img-fluid w-100">
+        </div>
+    
+        <div class="bg-dark-subtle m-auto rounded-bottom p-2 d-flex align-items-center">
+            <i class="icono bi bi-heart fs-2 mx-2"></i>
+            <i class="icono bi bi-chat fs-2 mx-2"></i>
+            <h4 class="tituloPost m-auto p-1">adrian teruel reina es mi nombre pero arroba pone messi liones</h4>
+        </div>
+
+    </div>
+    <div class="post container-fluid w-100 mx-auto mb-5"> {{-- Div Card --}}
+
+        <div class="bg-dark-subtle rounded-top">
+            <div class="d-flex align-items-center gap-3 ms-4">
+                <img src="img/default_profile/default_avatar.png" alt="imagenUsuario" width="50px" height="50px" class="img-fluid rounded-circle">
+                <div class="mt-3 d-flex flex-column align-items-center justify-content-center">
+                    <h5>Lionel Messi</h5>
+                    <p>19/abril/2023</p>
+                </div>
+            </div>
+        </div>
+    
+        <div class="m-auto">
+            <img src="img/default_profile/default_avatar.png" alt="imagen" class="img-fluid w-100">
+        </div>
+    
+        <div class="bg-dark-subtle m-auto rounded-bottom p-2 d-flex align-items-center">
+            <i class="icono bi bi-heart fs-2 mx-2"></i>
+            <i class="icono bi bi-chat fs-2 mx-2"></i>
+            <h4 class="tituloPost m-auto p-1">adrian teruel reina es mi nombre pero arroba pone messi liones</h4>
+        </div>
+
+    </div>
+    <div class="post container-fluid w-100 mx-auto mb-5"> {{-- Div Card --}}
+
+        <div class="bg-dark-subtle rounded-top">
+            <div class="d-flex align-items-center gap-3 ms-4">
+                <img src="img/default_profile/default_avatar.png" alt="imagenUsuario" width="50px" height="50px" class="img-fluid rounded-circle">
+                <div class="mt-3 d-flex flex-column align-items-center justify-content-center">
+                    <h5>Lionel Messi</h5>
+                    <p>19/abril/2023</p>
+                </div>
+            </div>
+        </div>
+    
+        <div class="m-auto">
+            <img src="img/default_profile/default_avatar.png" alt="imagen" class="img-fluid w-100">
+        </div>
+    
+        <div class="bg-dark-subtle m-auto rounded-bottom p-2 d-flex align-items-center">
+            <i class="icono bi bi-heart fs-2 mx-2"></i>
+            <i class="icono bi bi-chat fs-2 mx-2"></i>
+            <h4 class="tituloPost m-auto p-1">adrian teruel reina es mi nombre pero arroba pone messi liones</h4>
+        </div>
+
+    </div>
+    <div class="post container-fluid w-100 mx-auto mb-5"> {{-- Div Card --}}
+
+        <div class="bg-dark-subtle rounded-top">
+            <div class="d-flex align-items-center gap-3 ms-4">
+                <img src="img/default_profile/default_avatar.png" alt="imagenUsuario" width="50px" height="50px" class="img-fluid rounded-circle">
+                <div class="mt-3 d-flex flex-column align-items-center justify-content-center">
+                    <h5>Lionel Messi</h5>
+                    <p>19/abril/2023</p>
+                </div>
+            </div>
+        </div>
+    
+        <div class="m-auto">
+            <img src="img/default_profile/default_avatar.png" alt="imagen" class="img-fluid w-100">
+        </div>
+    
+        <div class="bg-dark-subtle m-auto rounded-bottom p-2 d-flex align-items-center">
+            <i class="icono bi bi-heart fs-2 mx-2"></i>
+            <i class="icono bi bi-chat fs-2 mx-2"></i>
+            <h4 class="tituloPost m-auto p-1">adrian teruel reina es mi nombre pero arroba pone messi liones</h4>
+        </div>
+
+    </div>
+    <div class="post container-fluid w-100 mx-auto mb-5"> {{-- Div Card --}}
+
+        <div class="bg-dark-subtle rounded-top">
+            <div class="d-flex align-items-center gap-3 ms-4">
+                <img src="img/default_profile/default_avatar.png" alt="imagenUsuario" width="50px" height="50px" class="img-fluid rounded-circle">
+                <div class="mt-3 d-flex flex-column align-items-center justify-content-center">
+                    <h5>Lionel Messi</h5>
+                    <p>19/abril/2023</p>
+                </div>
+            </div>
+        </div>
+    
+        <div class="m-auto">
+            <img src="img/default_profile/default_avatar.png" alt="imagen" class="img-fluid w-100">
+        </div>
+    
+        <div class="bg-dark-subtle m-auto rounded-bottom p-2 d-flex align-items-center">
+            <i class="icono bi bi-heart fs-2 mx-2"></i>
+            <i class="icono bi bi-chat fs-2 mx-2"></i>
+            <h4 class="tituloPost m-auto p-1">adrian teruel reina es mi nombre pero arroba pone messi liones</h4>
+        </div>
+
+    </div>
+    <div class="post container-fluid w-100 mx-auto mb-5"> {{-- Div Card --}}
+
+        <div class="bg-dark-subtle rounded-top">
+            <div class="d-flex align-items-center gap-3 ms-4">
+                <img src="img/default_profile/default_avatar.png" alt="imagenUsuario" width="50px" height="50px" class="img-fluid rounded-circle">
+                <div class="mt-3 d-flex flex-column align-items-center justify-content-center">
+                    <h5>Lionel Messi</h5>
+                    <p>19/abril/2023</p>
+                </div>
+            </div>
+        </div>
+    
+        <div class="m-auto">
+            <img src="img/default_profile/default_avatar.png" alt="imagen" class="img-fluid w-100">
+        </div>
+    
+        <div class="bg-dark-subtle m-auto rounded-bottom p-2 d-flex align-items-center">
+            <i class="icono bi bi-heart fs-2 mx-2"></i>
+            <i class="icono bi bi-chat fs-2 mx-2"></i>
+            <h4 class="tituloPost m-auto p-1">adrian teruel reina es mi nombre pero arroba pone messi liones</h4>
+        </div>
+
+    </div>
+    
 </div>
