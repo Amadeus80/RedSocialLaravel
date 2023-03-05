4aab2ff155519b832ee598dd13831e7465b5fba3
Pablo Acosta <acostaortizpablo@gmail.com>
2023-03-05 17:12:47 +0100

    añadido follows a la hora de recuperar

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\User;
+use App\Models\Follow;
+use Illuminate\Support\Facades\Auth;
 
 class UserController extends Controller
 {
@@ -11,7 +13,12 @@ class UserController extends Controller
         $usuarios = User::where("name", "like", "%$nombre%")->get();
         $listado = [];
         foreach ($usuarios as $usuario) {
+            $follow = false;
+            if(Follow::where("user_id", Auth::user()->id)->where("user_follow_id", $usuario->id)->first()){
+                $follow = true;
+            }
             $usuario->profile["name"] = $usuario->name;
+            $usuario->profile["follow"] = $follow;
             $listado[] = $usuario->profile;
         }
         return $listado;
