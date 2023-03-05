d6b08245a4318d6e9d3c67240a14ad1ddfcac0c5
Pablo Acosta <acostaortizpablo@gmail.com>
2023-03-05 18:28:32 +0100

    frontend perfiles

--- resources/views/perfil.blade.php
+++ resources/views/perfil.blade.php
@@ -3,5 +3,50 @@
 @section("title", "Perfil")
 
 @section('content')
-
+@vite(['resources/css/perfil.css'])
+    <div class="container bg-dark-subtle p-4 rounded contenedorPerfil d-flex flex-column gap-4">
+        <div class="w-75 m-auto d-flex align-items-center justify-content-between">
+            <div class="d-flex align-items-center gap-4">
+                <img src="{{asset("img/default_profile/default_avatar.png")}}" alt="imagen perfil" width="100px" height="100px" class="rounded-circle">
+                <h2>Pablo</h2>
+            </div>
+            <a href="#" class="btn btn-outline-success">Seguir</a>
+        </div>
+        <div class="d-flex justify-content-around">
+            <a class="w-25 btn btn-outline-dark">Post</a>
+            <a class="w-25 btn btn-outline-dark">Tus me gusta</a>
+            <a class="w-25 btn btn-outline-dark">Siguiendo</a>
+        </div>
+        <div class="container text-center border-top border-dark pt-3">
+            <div class="row">
+                {{-- POST --}}
+                <div class="col-12 col-md-4 mb-3">
+                    <img src="{{asset("img/default_profile/default_avatar.png")}}" alt="" class="img-fluid rounded">
+                </div>
+                <div class="col-12 col-md-4 mb-3">
+                    <img src="{{asset("img/default_profile/default_avatar.png")}}" alt="" class="img-fluid rounded">
+                </div>
+                <div class="col-12 col-md-4 mb-3">
+                    <img src="{{asset("img/default_profile/default_avatar.png")}}" alt="" class="img-fluid rounded">
+                </div>
+            
+                {{-- SIGUIENDO --}}
+                <div class="col-12 col-md-4 mb-3">
+                    <a href="#"><img src="{{asset("img/default_profile/default_avatar.png")}}" class="rounded-circle img-fluid" height="100" width="100"></a> <a href="#" class="text-decoration-none text-dark"><h3 class="mt-2">Pablo</h3></a>
+                </div>
+                <div class="col-12 col-md-4 mb-3">
+                    <a href="#"><img src="{{asset("img/default_profile/default_avatar.png")}}" class="rounded-circle img-fluid" height="100" width="100"></a> <a href="#" class="text-decoration-none text-dark"><h3 class="mt-2">Pablo</h3></a>
+                </div>
+                <div class="col-12 col-md-4 mb-3">
+                    <a href="#"><img src="{{asset("img/default_profile/default_avatar.png")}}" class="rounded-circle img-fluid" height="100" width="100"></a> <a href="#" class="text-decoration-none text-dark"><h3 class="mt-2">Pablo</h3></a>
+                </div>
+                <div class="col-12 col-md-4 mb-3">
+                    <a href="#"><img src="{{asset("img/default_profile/default_avatar.png")}}" class="rounded-circle img-fluid" height="100" width="100"></a> <a href="#" class="text-decoration-none text-dark"><h3 class="mt-2">Pablo</h3></a>
+                </div>
+                <div class="col-12 col-md-4 mb-3">
+                    <a href="#"><img src="{{asset("img/default_profile/default_avatar.png")}}" class="rounded-circle img-fluid" height="100" width="100"></a> <a href="#" class="text-decoration-none text-dark"><h3 class="mt-2">Pablo</h3></a>
+                </div>
+            </div>
+        </div>
+    </div>
 @endsection
--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import laravel from 'laravel-vite-plugin';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.scss', 'resources/js/app.js', 'resources/css/index.css', 'resources/css/nav.css', 'resources/css/post.css'],
+            input: ['resources/css/app.scss', 'resources/js/app.js', 'resources/css/index.css', 'resources/css/nav.css', 'resources/css/post.css', 'resources/css/perfil.css'],
             refresh: true,
         }),
     ],
