505dde57c12eadbcfe4fe6ad92847823245de38a
Pablo Acosta <acostaortizpablo@gmail.com>
2023-03-06 18:03:17 +0100

    cambio en el titulo del formulario de busqueda

--- resources/views/layout/components/nav.blade.php
+++ resources/views/layout/components/nav.blade.php
@@ -85,7 +85,7 @@
     <div class="modal-dialog modal-xl modal-dialog-centered modal-dialog-scrollable">
         <div class="modal-content">
             <div class="modal-header">
-                <h1 class="modal-title fs-5" id="exampleModalLabel">Busqueda</h1>
+                <h1 class="modal-title fs-5" id="exampleModalLabel">Busqueda de usuarios</h1>
                 <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
             <div class="modal-body">
