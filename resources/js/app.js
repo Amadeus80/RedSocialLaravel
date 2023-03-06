bc3a8125d817093c25fcbfa626325a2519e89be1
Pablo Acosta <acostaortizpablo@gmail.com>
2023-03-06 18:01:50 +0100

    arreglado peticiones ajax de espera

--- resources/js/app.js
+++ resources/js/app.js
@@ -7,49 +7,66 @@ window.$ = jQuery;
 $(function () {
     /* Hace una peticion ajax y dar like a algun post */
     let elemento;
-    let x = $( document );
     $(".likes").click(function ( event ){
         elemento = $(this);
         event.preventDefault();
-        x.ajaxStart(function(){
-            elemento.html(`<div class="spinner-border text-dark" role="status">
-            <span class="visually-hidden">Loading...</span>
-            </div>`);
-        });
-        $.get("http://127.0.0.1:8000/"+$(this).attr("href")+"/"+$(this).attr("id"), function (datos) {
-            if (elemento.attr("href")=="darLike") {
-                elemento.next().html(parseInt(elemento.next().html())+1);   
-                elemento.html(`<i class="icono bi bi-heart-fill fs-2 text-dark"></i>`);
-                elemento.attr("href", "quitarLike");
-            } else if((elemento.attr("href")=="quitarLike")) {
-                elemento.next().html(parseInt(elemento.next().html())-1);  
-                elemento.html(`<i class="icono bi bi-heart fs-2 text-dark"></i>`);
-                elemento.attr("href", "darLike");
+
+        $.ajax({
+            async:true,
+            type: "GET",
+            url: "http://127.0.0.1:8000/"+$(this).attr("href")+"/"+$(this).attr("id"),
+            beforeSend: function(){
+                elemento.html(`<div class="spinner-border text-dark" role="status">
+                <span class="visually-hidden">Loading...</span>
+                </div>`);
+            },
+            success: function(datos){
+                if (elemento.attr("href")=="darLike") {
+                    elemento.next().html(parseInt(elemento.next().html())+1);   
+                    elemento.html(`<i class="icono bi bi-heart-fill fs-2 text-dark"></i>`);
+                    elemento.attr("href", "quitarLike");
+                } else if((elemento.attr("href")=="quitarLike")) {
+                    elemento.next().html(parseInt(elemento.next().html())-1);  
+                    elemento.html(`<i class="icono bi bi-heart fs-2 text-dark"></i>`);
+                    elemento.attr("href", "darLike");
+                }
             }
         });
     })
 
     /* RECUPERA CON PETICION AJEX USUARIOS QUE TENGAN EN SU NOMBRE LA CADENA ESCRITA EN EL INPUT */
     $("#campoBusqueda").keyup(function(event){
+        let x = $( document );
         event.preventDefault();
         if(($(this).val()).length >= 2){
-            $.get(`http://127.0.0.1:8000/user/${$(this).val()}`, function(usuarios){
-                let data = "";
-                if(usuarios.length != 0){
-                    for (const usuario of usuarios) {
-                        let infoFollow = "No seguido";
-                        if(usuario.follow){
-                            infoFollow = "Seguido";
+            $.ajax({
+                async: true,
+                type: "GET",
+                dataType:"json",
+                url: `http://127.0.0.1:8000/user/${$(this).val()}`,
+                beforeSend : function(){
+                    $("#usuarios").html(`<div class="text-center"><div class="spinner-border text-dark fs-3" style="width: 5rem; height: 5rem;" role="status">
+                    <span class="visually-hidden">Loading...</span>
+                    </div></div>`);
+                },
+                success : function(usuarios){
+                    let data = "";
+                    if(usuarios.length != 0){
+                        for (const usuario of usuarios) {
+                            let infoFollow = "No seguido";
+                            if(usuario.follow){
+                                infoFollow = "Seguido";
+                            }
+                            data += `<div class="d-flex gap-2 align-items-center"><a href="http://127.0.0.1:8000/perfil/${usuario.user_id}"><img src="http://localhost:8000/${usuario.img}" class="rounded-circle" width="50" height="50"></a> <a href="http://127.0.0.1:8000/perfil/${usuario.user_id}" class="text-decoration-none text-dark">${usuario.name} - ${infoFollow}</a></div>`;
                         }
-                        data += `<div class="d-flex gap-2 align-items-center"><a href="http://127.0.0.1:8000/perfil/${usuario.user_id}"><img src="http://localhost:8000/${usuario.img}" class="rounded-circle" width="50" height="50"></a> <a href="http://127.0.0.1:8000/perfil/${usuario.user_id}" class="text-decoration-none text-dark">${usuario.name} - ${infoFollow}</a></div>`;
+
                     }
-                    
+                    else{
+                        data = "<h2 class='text-center'>Sin coincidencias...</h2>";
+                    }
+                    $("#usuarios").html(data);
                 }
-                else{
-                    data = "<h2 class='text-center'>Sin coincidencias...</h2>";
-                }
-                $("#usuarios").html(data);
-            })
+            });
         }
         else if(($(this).val()).length == 0){
             $("#usuarios").html("");
@@ -69,26 +86,32 @@ $(function () {
         $("#recuperarLikes").removeClass("active");
         $("#recuperarSiguiendo").removeClass("active");
 
-        x.ajaxStart(function(){
-            $("#contenedorInfoPerfiles").html(`<div class="text-center"><div class="spinner-border text-dark fs-3" style="width: 5rem; height: 5rem;" role="status">
-            <span class="visually-hidden">Loading...</span>
-            </div></div>`);
-        });
+        let elemento = $(this);
 
-        $.get(`http://127.0.0.1:8000/postPerfil/${$(this).attr("href")}`, function(datos){
-            let data = "";
-            if(datos.length > 0){
-                for (const dato of datos) {
-                    data += `<div class="col-12 col-md-4 mb-3">
-                        <a href="http://127.0.0.1:8000/post/${dato.id}"><img src="http://localhost:8000/${dato.img}" alt="" class="img-fluid rounded h-100 w-100"></a>
-                        </div>`
+        $.ajax({
+            async:true,
+            type: "GET",
+            url: `http://127.0.0.1:8000/postPerfil/${elemento.attr("href")}`,
+            beforeSend: function(){
+                $("#contenedorInfoPerfiles").html(`<div class="text-center"><div class="spinner-border text-dark fs-3" style="width: 5rem; height: 5rem;" role="status">
+                <span class="visually-hidden">Loading...</span>
+                </div></div>`);
+            },
+            success: function(datos){
+                let data = "";
+                if(datos.length > 0){
+                    for (const dato of datos) {
+                        data += `<div class="col-12 col-md-4 mb-3">
+                            <a href="http://127.0.0.1:8000/post/${dato.id}"><img src="http://localhost:8000/${dato.img}" alt="" class="img-fluid rounded h-100 w-100"></a>
+                            </div>`
+                    }
                 }
+                else{
+                    data = "<h2 class='p-3'>No hay posts</h2>";
+                }
+                $("#contenedorInfoPerfiles").html(data);
             }
-            else{
-                data = "<h2 class='p-3'>No hay posts</h2>";
-            }
-            $("#contenedorInfoPerfiles").html(data);
-        })
+        });
     });
 
     /* Recuperar likes */
@@ -97,27 +120,31 @@ $(function () {
         $(this).addClass("active");
         $("#recuperarPost").removeClass("active");
         $("#recuperarSiguiendo").removeClass("active");
-
-        x.ajaxStart(function(){
-            $("#contenedorInfoPerfiles").html(`<div class="text-center"><div class="spinner-border text-dark fs-3" style="width: 5rem; height: 5rem;" role="status">
-            <span class="visually-hidden">Loading...</span>
-            </div></div>`);
-        });
-
-        $.get(`http://127.0.0.1:8000/likesPerfil/${$(this).attr("href")}`, function(datos){
-            let data = "";
-            if(datos.length > 0){
-                for (const dato of datos) {
-                    data += `<div class="col-12 col-md-4 mb-3">
-                        <a href="http://127.0.0.1:8000/post/${dato.id}"><img src="http://localhost:8000/${dato.img}" alt="" class="img-fluid rounded h-100 w-100"></a>
-                        </div>`
+        let elemento = $(this);
+        $.ajax({
+            async:true,
+            type: "GET",
+            url: `http://127.0.0.1:8000/likesPerfil/${elemento.attr("href")}`,
+            beforeSend: function(){
+                $("#contenedorInfoPerfiles").html(`<div class="text-center"><div class="spinner-border text-dark fs-3" style="width: 5rem; height: 5rem;" role="status">
+                <span class="visually-hidden">Loading...</span>
+                </div></div>`);
+            },
+            success: function(datos){
+                let data = "";
+                if(datos.length > 0){
+                    for (const dato of datos) {
+                        data += `<div class="col-12 col-md-4 mb-3">
+                            <a href="http://127.0.0.1:8000/post/${dato.id}"><img src="http://localhost:8000/${dato.img}" alt="" class="img-fluid rounded h-100 w-100"></a>
+                            </div>`
+                    }
                 }
+                else{
+                    data = "<h2 class='p-3'>No hay ningún like</h2>";
+                }
+                $("#contenedorInfoPerfiles").html(data);
             }
-            else{
-                data = "<h2 class='p-3'>No hay ningún like</h2>";
-            }
-            $("#contenedorInfoPerfiles").html(data);
-        })
+        });
     });
 
     /* Recuperar siguiendo */
@@ -126,27 +153,33 @@ $(function () {
         $(this).addClass("active");
         $("#recuperarPost").removeClass("active");
         $("#recuperarLikes").removeClass("active");
+        
+        let elemento = $(this);
 
-        x.ajaxStart(function(){
-            $("#contenedorInfoPerfiles").html(`<div class="text-center"><div class="spinner-border text-dark fs-3" style="width: 5rem; height: 5rem;" role="status">
-            <span class="visually-hidden">Loading...</span>
-            </div></div>`);
-        });
-
-        $.get(`http://127.0.0.1:8000/siguiendoPerfil/${$(this).attr("href")}`, function(datos){
-            let data = "";
-            if(datos.length > 0){
-                for (const dato of datos) {
-                    data += `<div class="col-12 col-md-4 mb-3">
-                        <a href="http://127.0.0.1:8000/perfil/${dato.user_id}"><img src="http://localhost:8000/${dato.img}" class="rounded-circle" height="100" width="100"></a> <a href="#" class="text-decoration-none text-dark"><h3 class="mt-2">${dato.name}</h3></a>
-                        </div>`
+        $.ajax({
+            async:true,
+            type: "GET",
+            url: `http://127.0.0.1:8000/siguiendoPerfil/${elemento.attr("href")}`,
+            beforeSend: function(){
+                $("#contenedorInfoPerfiles").html(`<div class="text-center"><div class="spinner-border text-dark fs-3" style="width: 5rem; height: 5rem;" role="status">
+                <span class="visually-hidden">Loading...</span>
+                </div></div>`);
+            },
+            success: function(datos){
+                let data = "";
+                if(datos.length > 0){
+                    for (const dato of datos) {
+                        data += `<div class="col-12 col-md-4 mb-3">
+                            <a href="http://127.0.0.1:8000/perfil/${dato.user_id}"><img src="http://localhost:8000/${dato.img}" class="rounded-circle" height="100" width="100"></a> <a href="#" class="text-decoration-none text-dark"><h3 class="mt-2">${dato.name}</h3></a>
+                            </div>`
+                    }
                 }
+                else{
+                    data = "<h2 class='p-3'>No sigue a nadie</h2>";
+                }
+                $("#contenedorInfoPerfiles").html(data);
             }
-            else{
-                data = "<h2 class='p-3'>No sigue a nadie</h2>";
-            }
-            $("#contenedorInfoPerfiles").html(data);
-        })
+        });
     });
 
     /* OPCIONES FOLLOW */
@@ -178,5 +211,5 @@ $(function () {
 });
 
 /* TOOLTIP */
-const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
-const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+/* const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl)) */
