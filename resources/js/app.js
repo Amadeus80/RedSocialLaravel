0b000a26534785db14c7adb10b4f84dbec85ca81
AdrianTeru2001 <adrianteru2001@gmail.com>
2023-03-07 11:35:05 +0100

    Iconos perfil y tooltips

--- resources/js/app.js
+++ resources/js/app.js
@@ -211,5 +211,5 @@ $(function () {
 });
 
 /* TOOLTIP */
-/* const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
-const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl)) */
+const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
