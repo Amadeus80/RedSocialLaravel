3878f3691177c67975b82a41c4443664e165f50c
Pablo Acosta <acostaortizpablo@gmail.com>
2023-03-09 20:32:56 +0100

    comentarios gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,17 @@
 const { series, parallel } = require("gulp");
 const run = require("gulp-run");
 
-
+/* Realiza una migracion y un seeder */
 function migrate(){
     return run("php artisan migrate:fresh --seed").exec();
 }
 
+/* Arranca el servidor */
 function server(){
     return run("npm run serve").exec();
 }
 
+/* Arranca el servidor de vite */
 function dev(){
     return run("npm run dev").exec();
 }
@@ -17,6 +19,7 @@ function dev(){
 exports.migrate = migrate;
 exports.server = server;
 exports.dev = dev;
+
 exports.migrate_server = series(migrate, server);
 exports.dev_server = parallel(dev, server);
 
